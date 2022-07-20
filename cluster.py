766fced379d069a0c8a59f3a0b1fe4bda7dadd6a
Mehran Asaad <mehran.asaad@gmail.com>
2022-07-20 22:19:42 +0430

    init

---
+++ cluster.py
@@ -0,0 +1,174 @@
+from kubernetes import client, config
+from kubernetes.stream import stream
+
+from collections import defaultdict
+import random
+import time
+import json
+import yaml
+
+try:
+    config.load_incluster_config()
+except:
+    config.load_kube_config()
+    
+class Node:
+    def __init__(self, id, name, spec_cpu, spec_mem, inuse_cpu=0, inuse_mem=0):
+        self.id = id
+        self.name = name
+        self.spec_cpu = spec_cpu
+        self.spec_mem = spec_mem
+        self.inuse_cpu = inuse_cpu    
+        self.inuse_mem = inuse_mem
+    
+    def __repr__(self) -> str:
+        return f"Node(ID: {self.id}, Spec: <{self.spec_cpu}, {self.spec_mem}>)"
+        
+    def get_rem_cpu(self):
+        return self.spec_cpu - self.inuse_cpu
+    
+    def get_rem_mem(self):
+        return self.spec_mem - self.inuse_mem
+    
+                    
+class Container:
+    def __init__(self, id, name, app_name, req_cpu, req_mem):
+        self.id = id
+        self.name = name
+        self.app_name = app_name
+        self.req_cpu = req_cpu
+        self.req_mem = req_mem
+    
+    def __repr__(self) -> str:
+        return f"Container(ID: {self.id}, App: {self.app_name}, req: <{self.req_cpu}, {self.req_mem}>)"    
+
+def get_all_simul_nodes():
+    nodes = []
+    possible_cpu = range(1,9)
+    possible_mem = [1000, 2000, 4000, 8000, 10000, 12000]
+    for i in range(1,6):
+        nodes.append(Node("n"+str(i), "n"+str(i), random.choice(possible_cpu), random.choice(possible_mem)))
+        
+    return nodes
+
+def get_all_simul_containers():
+    containers = []
+    possible_req_cpu = range(1,5)
+    possible_req_mem = range(100, 4000, 200)
+    possible_apps = ["app1", "app2"]
+    for i in range(1,7):
+        containers.append(Container("c"+str(i), "c"+str(i), random.choice(possible_apps), random.choice(possible_req_cpu), random.choice(possible_req_mem)))
+        
+    return containers
+
+def get_nodes():
+    v1 = client.CoreV1Api()
+    cust = client.CustomObjectsApi()
+
+    metrics = cust.list_cluster_custom_object('metrics.k8s.io', 'v1beta1', 'nodes')
+
+    cluster_nodes = defaultdict(dict)
+    
+    # n = nano cores, Ki = kilobytes
+    for stats in metrics['items']:
+        node_name = stats['metadata']['name']
+        cluster_nodes[node_name]['used_cpu'] = float(stats['usage']['cpu'][:-1]) / 10**9
+        cluster_nodes[node_name]['used_memory'] = float(stats['usage']['memory'][:-2]) / 10**3
+        
+        
+    for node in v1.list_node().items:
+        node_name = node.status.addresses[1].address
+        cluster_nodes[node_name]['spec_cpu'] = float(node.status.allocatable['cpu'])
+        cluster_nodes[node_name]['spec_memory'] = float(node.status.allocatable['memory'][:-2]) / 10**3
+    
+    nodes = []
+    for id, (name, info) in enumerate(cluster_nodes.items(), 1):
+        node = Node('n'+str(id), name, info['spec_cpu'], info['spec_memory'], info['used_cpu'], info['used_memory'])
+        nodes.append(node)
+        
+    return nodes
+
+def get_pending_containers():
+    v1 = client.CoreV1Api()
+    containers = []
+    
+    # cpu in mili, memory in Mi
+    for id, pod in enumerate(v1.list_pod_for_all_namespaces().items, 1):
+        if pod.status.phase == 'Pending':
+            pod_name = pod.metadata.name
+            app_name = pod.metadata.labels['app']
+            req_cpu = float(pod.spec.containers[0].resources.requests['cpu'][:-1]) / 1000
+            req_memory = float(pod.spec.containers[0].resources.requests['memory'][:-2])
+            containers.append(Container('c'+str(id), pod_name, app_name, req_cpu, req_memory))
+        
+    return containers
+
+def pod_exec(name, namespace, command, api_instance):
+    exec_command = ["/bin/sh", "-c", command]
+
+    resp = stream(api_instance.connect_get_namespaced_pod_exec,
+                  name,
+                  namespace,
+                  command=exec_command,
+                  stderr=True, stdin=False,
+                  stdout=True, tty=False,
+                  _preload_content=False)
+
+    while resp.is_open():
+        resp.update(timeout=1)
+        if resp.peek_stdout():
+            # print(f"STDOUT: \n{resp.read_stdout()}")
+            output = resp.read_stdout()
+        if resp.peek_stderr():
+            print(f"STDERR: \n{resp.read_stderr()}")
+
+    resp.close()
+
+    if resp.returncode != 0:
+        raise Exception("Script failed")
+    
+    return output
+
+def get_network_bitrate():
+    # bitrate (Gbits/sec) of each node to master
+    
+    v1_a = client.AppsV1Api()
+    v1_b = client.CoreV1Api()
+    with open('iperf3.yaml','r') as ymlfile:
+        data = yaml.safe_load_all(ymlfile)
+        dep, serv, dmset = data
+        
+    v1_a.create_namespaced_deployment(body=dep, namespace='default')
+    v1_b.create_namespaced_service(body=serv, namespace='default')
+    v1_a.create_namespaced_daemon_set(body=dmset, namespace='default')
+    
+    time.sleep(2)
+    clients = []
+    pod_items = v1_b.list_namespaced_pod('default').items
+    for pod in pod_items:
+        if pod.metadata.labels.get('app') == 'iperf3-client':
+            clients.append({'name':pod.metadata.name, 'node': pod.spec.node_name, 'ip':None})
+
+    while not all([cli['ip'] for cli in clients]):
+        for pod in clients:
+            if not pod['ip']:
+                stat = v1_b.read_namespaced_pod(pod['name'], namespace='default')
+                if prob := stat.status.container_statuses:
+                    if prob[0].ready:
+                        pod['ip'] = stat.status.host_ip
+
+    for cli in clients:
+        cmd = f"iperf3 -c iperf3-server --json -T 'Client on {cli['ip']}'"
+        while True:
+            try:
+                output = json.loads(pod_exec(cli['name'], "default", cmd, v1_b))
+                break
+            except:
+                pass
+        cli['bitrate'] = output['end']['sum_received']['bits_per_second'] / 10**9  #Gbits/sec
+        
+    v1_a.delete_namespaced_deployment(name=dep['metadata']['name'], namespace='default')
+    v1_b.delete_namespaced_service(name=serv['metadata']['name'], namespace='default')
+    v1_a.delete_namespaced_daemon_set(name=dmset['metadata']['name'], namespace='default')
+    
+    return [{'node':n['node'], 'bitrate':n['bitrate']} for n in clients]
---
+++ nsgaiii.py
@@ -0,0 +1,306 @@
+from pymoo.core.duplicate import ElementwiseDuplicateElimination
+from pymoo.factory import get_reference_directions, get_selection
+from pymoo.operators.selection.tournament import compare
+from pymoo.core.problem import ElementwiseProblem
+from pymoo.visualization.scatter import Scatter
+from pymoo.algorithms.moo.nsga3 import NSGA3
+from pymoo.core.crossover import Crossover
+from pymoo.core.sampling import Sampling
+from pymoo.core.mutation import Mutation
+from pymoo.optimize import minimize
+from pyrecorder.recorder import Recorder
+from pyrecorder.writers.video import Video
+
+import numpy as np
+from numpy.random import default_rng
+rng = default_rng()
+
+from collections import Counter, defaultdict
+
+from cluster import get_all_simul_nodes, get_all_simul_containers
+
+
+
+nodes = {n.id: n for n in get_all_simul_nodes()}
+containers = {c.id: c for c in get_all_simul_containers()}
+        
+def objective_1(x):
+    x = x.replace('0','')
+    count = Counter(x)
+    return sum([a*(a+1)/2 for a in count.values()])
+
+def objective_2(x):
+    ans_sum = 0
+    nds = defaultdict(list)
+    for i, e in enumerate(x):
+        if e != '0':
+            nds[e].append(containers['c'+str(i+1)].app_name)
+            
+    for typelist in nds.values():
+        c = Counter(typelist)
+        ans_sum += sum([a*(a+1)/2 for a in c.values()])
+    
+    return ans_sum
+
+def objective_3(x):
+    
+    def get_power(node_cpu, node_memory):
+        PCPU = 0.3
+        PMEM = 0.11
+        PIDLE_TOTAL_REF = 171
+        PMAX_TOTAL_REF = 218
+        CORES_REF = 4
+        MEM_REF = 4000
+        
+        pidle_cpu_ref = PIDLE_TOTAL_REF * PCPU
+        pidle_mem_ref = PIDLE_TOTAL_REF * PMEM
+        delta_pidle_cpu = (pidle_cpu_ref * node_cpu / CORES_REF) - pidle_cpu_ref
+        delta_pidle_mem = (pidle_mem_ref * node_memory / MEM_REF) - pidle_mem_ref
+        pidle_total = PIDLE_TOTAL_REF + delta_pidle_cpu + delta_pidle_mem
+    
+        pmax_cpu_ref = PMAX_TOTAL_REF * PCPU
+        pmax_mem_ref = PMAX_TOTAL_REF * PMEM
+        delta_pmax_cpu = (pmax_cpu_ref * node_cpu / CORES_REF) - pmax_cpu_ref
+        delta_pmax_mem = (pmax_mem_ref * node_memory / MEM_REF) - pmax_mem_ref
+        pmax_total = PMAX_TOTAL_REF + delta_pmax_cpu + delta_pmax_mem
+    
+        return pidle_total, pmax_total
+    
+    all_power = 0
+    nds = defaultdict(list)
+    for i, e in enumerate(x):
+        if e != '0':
+            nds[e].append([containers['c'+str(i+1)].req_cpu, containers['c'+str(i+1)].req_mem])
+    
+    for nid, usage in nds.items():
+        cntrs_use_cpu = sum([a[0] for a in usage])
+        cntrs_use_mem = sum([a[1] for a in usage])
+        
+        cpu_usage_util = (nodes['n'+nid].inuse_cpu + cntrs_use_cpu) / nodes['n'+nid].spec_cpu
+        mem_usage_util = (nodes['n'+nid].inuse_mem + cntrs_use_mem) / nodes['n'+nid].spec_mem
+        
+        idle_power, max_power = get_power(nodes['n'+nid].spec_cpu, nodes['n'+nid].spec_mem)
+        
+        all_power += (max_power - idle_power) * (cpu_usage_util + mem_usage_util)/2 + idle_power
+        
+    return all_power
+
+def objective_4(x):
+    nds = defaultdict(list)
+    nds_free_cpu_percs = []
+    nds_free_mem_percs = []
+    for i, e in enumerate(x):
+        if e != '0':
+            nds[e].append([containers['c'+str(i+1)].req_cpu, containers['c'+str(i+1)].req_mem])
+
+    for nid, usage in nds.items():
+        cntrs_use_cpu = sum([a[0] for a in usage])
+        cntrs_use_mem = sum([a[1] for a in usage])
+        
+        free_cpu_perc = abs(nodes['n'+nid].get_rem_cpu() - cntrs_use_cpu)/nodes['n'+nid].spec_cpu
+        free_mem_perc = abs(nodes['n'+nid].get_rem_mem() - cntrs_use_mem)/nodes['n'+nid].spec_mem
+        
+        nds_free_cpu_percs.append(free_cpu_perc)
+        nds_free_mem_percs.append(free_mem_perc)
+        
+    return sum([abs(a-b) for a,b in zip(nds_free_cpu_percs, nds_free_mem_percs)])/len(nodes) * 100
+
+def objective_5(x):
+    return x.count('0')
+
+def feasibility(x):
+    constraint_violation = 0
+    constraint_sum_cpu = 0
+    constraint_sum_mem = 0
+    inuse_nodes = [[0,0] for _ in range(len(nodes))]
+    for container_id, node_id in enumerate(x, 1):
+        if node_id != '0':
+            inuse_nodes[int(node_id)-1][0] += containers['c'+str(container_id)].req_cpu
+            inuse_nodes[int(node_id)-1][1] += containers['c'+str(container_id)].req_mem
+        
+    for nid, (ncpu, nmem) in enumerate(inuse_nodes, 1):
+        rem_cpu = nodes['n'+str(nid)].get_rem_cpu()
+        rem_mem = nodes['n'+str(nid)].get_rem_mem()
+        if ncpu > rem_cpu or nmem > rem_mem:
+            constraint_violation += 1
+            if ncpu > rem_cpu:
+                constraint_sum_cpu += ncpu - rem_cpu
+            if nmem > rem_mem:
+                constraint_sum_mem += nmem - rem_mem
+            
+    return constraint_violation, constraint_sum_cpu, constraint_sum_mem
+
+
+class MyProblem(ElementwiseProblem):
+
+    def __init__(self, n_characters=len(containers), **kargs):
+        super().__init__(n_var=1, n_obj=5, n_constr=1)
+        self.n_characters = n_characters
+        self.ALPHABET = [str(c+1) for c in range(len(nodes))]
+
+    def _evaluate(self, X, out, *args, **kwargs):
+        X = X[0]
+        all_objectives = [objective_1, objective_2, objective_3, objective_4, objective_5]
+        out["F"] = [obj_func(X) for obj_func in all_objectives]
+        out["G"] = list(feasibility(X)[0:1])
+        # print(X, [int(a) for a in out['F']], '|',list(feasibility(X)))
+        
+class MySampling(Sampling):
+
+    def _do(self, problem, n_samples, **kwargs):
+        X = np.full((n_samples, 1), None, dtype=object)
+
+        for i in range(n_samples):
+            X[i, 0] = "".join([rng.choice(problem.ALPHABET) for _ in range(problem.n_characters)])
+
+        return X
+    
+class MyCrossover(Crossover):
+    def __init__(self):
+
+        # define the crossover: number of parents and number of offsprings
+        super().__init__(2, 1)
+
+    def _do(self, problem, X, **kwargs):
+
+        # The input of has the following shape (n_parents, n_matings, n_var)
+        _, n_matings, n_var = X.shape
+
+        Y = np.full((1, n_matings, 1), None, dtype=object)
+
+        # for each mating provided
+        for k in range(n_matings):
+
+            # get the first and the second parent
+            a, b = X[0, k, 0], X[1, k, 0]
+
+            single_point = rng.integers(1, problem.n_characters)
+            offspring = a[:single_point] + b[single_point:] 
+            
+            # set the output
+            Y[0, k, 0] = offspring
+
+        return Y
+    
+class MyMutation(Mutation):
+    def __init__(self):
+        super().__init__()
+
+    def _do(self, problem, X, **kwargs):
+
+        X = X[:int(len(X)/2)]
+        
+        # for each individual
+        for i in range(len(X)):
+            if rng.random() <= 0.5: # OR 0.3
+                
+                r = rng.random()
+            
+                if r <= 0.25: # change
+                    ind = list(X[i, 0])
+                    while (ch_index := rng.integers(0, problem.n_characters)) != '0':
+                        psbl_char = list(set(problem.ALPHABET) - set(ind[ch_index]))
+                        ind[ch_index] = rng.choice(psbl_char)
+                        break 
+                    
+                    X[i, 0] = "".join(ind)
+                
+                elif (r > 0.25 and r <= 0.5): # swap
+                    while True:
+                        a, b = rng.integers(0, problem.n_characters, 2)
+                        if a != b:
+                            ind = list(X[i, 0])
+                            ind[a], ind[b] =  ind[b], ind[a]
+                            X[i, 0] = "".join(ind)
+                            break
+                        
+                elif (r > 0.5 and r <= 0.51):
+                    if not feasibility(X[i, 0])[0]: # unassignAssigned
+                        ind = list(X[i, 0])
+                        mut_index = rng.integers(0, problem.n_characters)
+                        if ind[mut_index] != "0":
+                            ind[mut_index] = "0"
+                            X[i, 0] = "".join(ind)
+                            
+                elif (r > 0.51 and r <= 1.0):
+                    ind = list(X[i, 0])
+                    none_indexes = []
+                    if ind.count("0") > 0: # assignUnassigned
+                        for index, j in enumerate(ind):
+                            if j == "0":
+                                none_indexes.append(index)
+                        
+                        ind[rng.choice(none_indexes)] = rng.choice(problem.ALPHABET)
+                        X[i, 0] = "".join(ind)
+        return X
+    
+class MyDuplicateElimination(ElementwiseDuplicateElimination):
+    def is_equal(self, a, b):
+        return a.X[0] == b.X[0]
+
+def mybinary_tournament(pop, P, **kwargs):
+
+    S = np.full(P.shape[0], np.nan)
+
+    for i in range(P.shape[0]):
+        a, b = P[i, 0], P[i, 1]
+
+        # if at least one solution is infeasible
+        if pop[a].CV > 0.0 or pop[b].CV > 0.0:
+            S[i] = compare(a, pop[a].CV, b, pop[b].CV, method='smaller_is_better', return_random_if_equal=True)
+
+        # both solutions are feasible just set random
+        elif pop[a].F[4] < pop[b].F[4]:
+            S[i] = a
+        else:
+            S[i] = b
+
+    return S[:, None].astype(int)
+
+def record_generation_video(minimize_result):
+    with Recorder(Video("ga.mp4", fps=5)) as rec:
+
+        # for each algorithm object in the history
+        for entry in minimize_result.history:
+            sc = Scatter(title=("Gen %s" % entry.n_gen))
+            sc.add(entry.pop.get("F"), s=10)
+            sc.add(entry.problem.pareto_front(), plot_type="line", color="black", alpha=0.7)
+            sc.do()
+
+            # finally record the current visualization to the video
+            rec.record()
+
+
+def main():
+        
+    # (5, 6) = (M, p) = (n_obj, #of divisions) -> H = C(M+p-1, p)
+    ref_dirs = get_reference_directions("das-dennis", 5, n_partitions=6)
+    algorithm = NSGA3(pop_size=212,
+                    sampling=MySampling(),
+                    selection=get_selection('tournament', func_comp=mybinary_tournament),
+                    crossover=MyCrossover(),
+                    mutation=MyMutation(),
+                    eliminate_duplicates=MyDuplicateElimination(),
+                    ref_dirs=ref_dirs)
+
+    res = minimize(MyProblem(),
+                algorithm,
+                ('n_gen', 100),
+                seed=1,
+                save_history=True,
+                return_least_infeasible=True,
+                verbose=False)
+
+    Scatter(tight_layout=True).add(res.F, s=10).show()
+
+    results = res.X[np.argsort(res.F[:, 0])]
+    print(results)
+    print("Exec Time:", res.exec_time)
+
+    print(nodes)
+    print(containers)
+    
+    record_generation_video(res)
+    
+if __name__ == "__main__":
+    main()
